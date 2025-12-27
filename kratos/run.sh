6409b402c03025f9fafbf86afd53de9d466fa8a0
omerRastgar <or05554@st.habib.edu.pk>
2025-12-27 16:45:43 +0500

    add

--- kratos/run.sh
+++ kratos/run.sh
@@ -9,6 +9,8 @@ echo "Generating /etc/config/kratos/kratos.yml from template..."
 sed -e "s|\${DSN}|$DSN|g" \
     -e "s|\${GOOGLE_CLIENT_ID}|$GOOGLE_CLIENT_ID|g" \
     -e "s|\${GOOGLE_CLIENT_SECRET}|$GOOGLE_CLIENT_SECRET|g" \
+    -e "s|\${PUBLIC_URL}|$PUBLIC_URL|g" \
+    -e "s|\${COOKIE_DOMAIN}|$COOKIE_DOMAIN|g" \
     /etc/config/kratos/kratos.template.yml > /tmp/kratos.yml
 
 echo "Configuration generated. Starting Kratos..."
