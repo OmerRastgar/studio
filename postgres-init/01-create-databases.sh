0e5b241cce27a813d1e211560a83d65580ad9200
omerRastgar <or05554@st.habib.edu.pk>
2025-12-27 17:56:42 +0500

    Fix: Make init script idempotent

--- postgres-init/01-create-databases.sh
+++ postgres-init/01-create-databases.sh
@@ -4,11 +4,17 @@ set -u
 
 function create_user_and_database() {
 	local database=$1
-	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
+	echo "  Processing database '$database'..."
+	exists=$(psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -tAc "SELECT 1 FROM pg_database WHERE datname = '$database'")
+	if [ "$exists" = "1" ]; then
+		echo "  Database '$database' already exists. Skipping."
+	else
+		echo "  Creating user and database '$database'"
+		psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+		    CREATE DATABASE $database;
+		    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
+	fi
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
